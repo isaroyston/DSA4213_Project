07536274a47f6b822c06698052bf930390091260
isaroyston <146667167+isaroyston@users.noreply.github.com>
2025-10-02 19:54:56 +0800

    Create BERTopic variant 1 script

---
+++ src/models/bertopic_variant1.py
@@ -0,0 +1,8 @@
+# BERTopic variant 1 script placeholder
+
+def train_bertopic_variant1():
+    # Import and training steps go here
+    pass
+
+if __name__ == "__main__":
+    train_bertopic_variant1()
