085ef1c15d4d868f392e5eba1c184a7dc4520ea0
isaroyston <146667167+isaroyston@users.noreply.github.com>
2025-10-02 19:57:52 +0800

    own_embeddings

--- src/models/bertopic_variant1.py
+++
@@ -1,8 +0,0 @@
-# BERTopic variant 1 script placeholder
-
-def train_bertopic_variant1():
-    # Import and training steps go here
-    pass
-
-if __name__ == "__main__":
-    train_bertopic_variant1()
